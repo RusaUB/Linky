f84c9c071de41c64565c7fd544df1dfdd8b8b8c2
Your Name <you@example.com>
2023-06-08 20:38:00 +0200

    pre-save

--- screens/explore/ExploreScreen.jsx
+++ screens/explore/ExploreScreen.jsx
@@ -5,14 +5,13 @@ import EventCard from '../../components/explore/EventCard';
 import LocationCard from '../../components/explore/LocationCard';
 import CardInline from '../../components/explore/CardInline';
 import Octicons from 'react-native-vector-icons/Octicons';
-import {IconButton, MD3Colors} from 'react-native-paper';
 import CategotyCard from '../../components/explore/CategotyCard';
 
 const ExploreScreen = () => {
   return (
     <SafeAreaView className="bg-white">
-      <ScrollView className="p-3 bg-white">
-        <View className="mt-2 mb-7">
+      <ScrollView className=" bg-white" showsVerticalScrollIndicator={false}>
+        <View className="mt-2 mb-1 p-3">
           <Text className="font-extralight text-xs mb-1">Find events in</Text>
           <LocationCard
             fontSize={20}
@@ -22,18 +21,18 @@ const ExploreScreen = () => {
           />
         </View>
 
-        <View>
+        <View className = 'p-3'>
           <EventCard />
         </View>
 
-        <ScrollView horizontal = {true} className = 'my-5' showsHorizontalScrollIndicator = {false}>
+        <ScrollView horizontal = {true} className = 'my-5 pl-3' showsHorizontalScrollIndicator = {false}>
           <CategotyCard style={{marginRight : 10}} />
           <CategotyCard style={{marginRight : 10}} />
           <CategotyCard style={{marginRight : 10}} />
           <CategotyCard style={{marginRight : 10}} />
         </ScrollView>
 
-        <View>
+        <View className = "p-3">
           <View className="flex-row justify-between items-center">
             <Text className="text-base font-medium">Upcoming Events</Text>
             <TouchableOpacity className="flex-row items-center space-x-1 opacity-30">
