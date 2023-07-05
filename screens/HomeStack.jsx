71c90cfa236fe00778854fd1c94008b9330647ea
Your Name <you@example.com>
2023-07-05 16:41:52 +0500

    animated bar not finished

--- screens/HomeStack.jsx
+++ screens/HomeStack.jsx
@@ -1,32 +1,66 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-
 import AddScreen from './add/AddScreen';
 import MapScreen from './map/MapScreen';
 import ProfileScreen from './profile/ProfileScreen';
 import HomeScreen from './home/HomeScreen';
 
 import Icon from 'react-native-vector-icons/Ionicons';
-import { TypographyColors } from '../constants/theme';
+import {TypographyColors} from '../constants/theme';
 
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import MessageScreen from './message/MessageScreen';
 
+import LottieView from 'lottie-react-native';
+import {useEffect, useRef} from 'react';
+
+import HomeAnimation from '../assets/icons/home.json';
+import MessageAnimation from '../assets/icons/chat.json';
+import ProfileAnimation from '../assets/icons/profile-navbar.json';
+import MapAnimation from '../assets/icons/map.json';
+
+import {Animated} from 'react-native';
+import {Image} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 function HomeStack() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        
+        tabBarIcon: ({size, focused}) => {
+          let sourceFile;
+          let isIcon = true;
+          switch (route.name) {
+            case 'HomeScreen':
+              sourceFile = HomeAnimation;
+              break;
+            case 'Map':
+              sourceFile = MapAnimation;
+              break;
+            case 'Message':
+              sourceFile = MessageAnimation;
+              break;
+            case 'Profile':
+              isIcon = false;
+              break;
+          }
+          return isIcon ? (
+            <LottieView source={sourceFile} loop={false} autoPlay={focused} />
+          ) : (
+            <Image
+              source={require('../assets/images/avatar-male.jpg')}
+              className="w-[30] h-[30] rounded-full"
+            />
+          );
+        },
+      })}>
       <Tab.Screen
         name="HomeScreen"
         component={HomeScreen}
         headermode="none"
         options={{
           headerShown: false,
-          tabBarIcon: ({color = TypographyColors.purple, size}) => (
-            <Icon name="compass" color={color} size={size} />
-          ),
           tabBarActiveTintColor: TypographyColors.purple,
           tabBarLabel: () => null,
         }}
@@ -35,9 +69,6 @@ function HomeStack() {
         name="Map"
         component={MapScreen}
         options={{
-          tabBarIcon: ({color, size}) => (
-            <Icon name="location" color={color} size={(size = 24)} />
-          ),
           tabBarActiveTintColor: TypographyColors.purple,
           tabBarLabel: () => null,
         }}
@@ -56,14 +87,12 @@ function HomeStack() {
       <Tab.Screen
         name="Message"
         component={MessageScreen}
+        listeners={{
+          tabPress: () => {
+            // Make lottieView of message to play
+          },
+        }}
         options={{
-          tabBarIcon: ({color, size}) => (
-              <MaterialCommunityIcons
-                color={color}
-                name="message"
-                size={24}
-              />
-          ),
           tabBarActiveTintColor: TypographyColors.purple,
           headerShown: false,
           tabBarLabel: () => null,
@@ -73,10 +102,6 @@ function HomeStack() {
         name="Profile"
         component={ProfileScreen}
         options={{
-          tabBarIcon: ({color, size}) => (
-            <Icon name="person" color={color} size={(size = 24)} />
-          ),
-          tabBarActiveTintColor: TypographyColors.purple,
           tabBarLabel: () => null,
         }}
       />
